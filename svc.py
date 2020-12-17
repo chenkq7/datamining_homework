871e5597fb54b131f95291a674b002d3ab921bb5
cheney <17231055@buaa.edu.cn>
2020-12-17 20:36:57 +0800

    TODO: save/load model.

--- svc.py
+++ svc.py
@@ -1,4 +1,39 @@
 import numpy as np
+import pandas as pd
+
+
+def nominal2binary(data, nominals):
+    """
+    :param data: pd.DataFrame.
+    :param nominals: columns whose data type is Nominal.
+    :return: convert Nominal data type to one-hot binary data type.
+    """
+    assert isinstance(data, pd.DataFrame)
+    for nominal in nominals:
+        max_value = max(data[nominal])
+        if max_value >= 2:
+            for v in range(max_value + 1):
+                data.loc[:, nominal + '_' + str(v)] = (data.loc[:, nominal] == v).astype(int)
+    columns = []
+    for col in data.columns.values:
+        if col not in nominals:
+            columns.append(col)
+    return data[columns]
+
+
+def f1_score(y_true, y_pred, pos_label=1, labels=None):
+    y_true = np.asarray(y_true)
+    y_pred = np.asarray(y_pred)
+    if labels is None:
+        labels = set(y_true)
+    assert not set(y_true).difference(labels)
+    assert not set(y_pred).difference(labels)
+    pos_pred = (y_pred == pos_label).sum()
+    pos_true = (y_true == pos_label).sum()
+    tp = np.logical_and(y_true == pos_label, y_pred == pos_label).sum()
+    precision = tp / pos_pred if pos_pred else 0
+    recall = tp / pos_true if pos_true else 0
+    return 2 * precision * recall / (precision + recall) if precision + recall else 0
 
 
 class Kernel:
@@ -6,9 +41,9 @@ class Kernel:
     def linear(sigma):
         return lambda x, y: np.inner(x, y)
 
-    @staticmethod
-    def gaussian(sigma):
-        return lambda x, y: np.exp(-np.sqrt(np.linalg.norm(x - y) ** 2 / (2 * sigma ** 2)))
+    # @staticmethod
+    # def gaussian(sigma):
+    #     return lambda x, y: np.exp(-np.sqrt(np.linalg.norm(x - y) ** 2 / (2 * sigma ** 2)))
 
     @classmethod
     def dispatch(cls, name, sigma):
@@ -83,7 +118,8 @@ class SVC:
         err[np.logical_and(cond1, err > 0)] = 0
         err[np.logical_and(cond2, err == 0)] = 0
         err[np.logical_and(cond3, err < 0)] = 0
-        err = err ** 2
+        err = self._alpha * err + self.C * np.maximum(-err, 0)
+        err = np.abs(err)
         idx = np.argmax(err)
         if self.verbose:
             print(err[idx])
@@ -109,6 +145,8 @@ class SVC:
         E1 = self._predict_cache[idx1] - y1
         E2 = self._predict_cache[idx2] - y2
         eta = self._gram[idx1][idx1] + self._gram[idx2][idx2] - 2 * self._gram[idx1][idx2]
+        if eta == 0:
+            return
         alpha2_new_unc = self._alpha[idx2] + y2 * (E1 - E2) / eta
         #
         alpha2_new = np.clip(alpha2_new_unc, l, h)
@@ -148,13 +186,50 @@ class SVC:
 
 
 if __name__ == '__main__':
-    x = np.array([[0, 0], [1, 1], [0, 1], [1, 0], [2, 0]])
-    y = np.array([-1, 1, -1, -1, 1])
+    data = pd.read_csv("./dataset/线下/svm/svm_training_set.csv")
+    idx_train = np.random.choice(len(data), int(9 / 10 * len(data)))
+    idx_val = list(set(list(range(len(data)))).difference(idx_train))
+    nominals = ['x1', 'x4', 'x6', 'x7', 'x8', 'x9']
+    ord_rat = ['x5', 'x2', 'x3', 'x10', 'x11', 'x12']
+    x_train = nominal2binary(data.iloc[idx_train, :12], nominals=nominals)
+    x_val = nominal2binary(data.iloc[idx_val, :12], nominals=nominals)
+    y_train = data.iloc[idx_train, 12]
+    y_val = data.iloc[idx_val, 12]
 
-    from sklearn import svm
+    y_neg_idx = np.arange(len(y_train))[y_train < 0]
+    y_neg_idx_sam = np.random.choice(y_neg_idx, 6000, replace=False)
+    y_pos_idx_sam = np.arange(len(y_train))[y_train > 0]
+    idx_sample = np.concatenate((y_neg_idx_sam, y_pos_idx_sam))
+    idx_sample = np.random.choice(idx_sample, 2000, replace=False)
 
-    svc = SVC(c=100, verbose=True)
-    # svc = svm.SVC(C=100, kernel='linear')
-    svc.fit(x, y)
-    print(svc.predict(x))
-    # print(svc.fit_reach_epochs_end)
+    x_train = x_train.iloc[idx_sample]
+    y_train = y_train.iloc[idx_sample]
+
+    for col in x_train.columns.values:
+        if str(col).startswith(tuple(nominals)):
+            max_v = np.max(x_train[col])
+            min_v = np.min(x_train[col])
+            if max_v != min_v:
+                x_train[col] = (x_train[col] - min_v) / (max_v - min_v)
+                x_val[col] = (x_val[col] - min_v) / (max_v - min_v)
+        else:
+            mean = np.mean(x_train[col])
+            std = np.std(x_train[col])
+            x_train[col] = (x_train[col] - mean) / std
+            x_val[col] = (x_val[col] - mean) / std
+
+    print(pd.DataFrame(x_train).columns.values)
+    print(pd.DataFrame(y_train).columns.values)
+
+    svc = SVC(c=1, verbose=True)
+    svc.fit(x_train, y_train, epochs=int(1e5))
+
+    val_pred = svc.predict(x_val)
+    print(np.asarray(val_pred == y_val).mean())
+
+    from sklearn import metrics
+
+    print(f1_score(y_val, val_pred))
+    print(metrics.f1_score(y_val, val_pred))
+    print(metrics.recall_score(y_val, val_pred))
+    print(metrics.accuracy_score(y_val, val_pred))
